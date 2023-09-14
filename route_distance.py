892443f44fb5c6790e52245e9100b06fdf1cb87b
Kushagra Khatwani <kushagrakhatwani19@gmail.com>
2023-09-14 15:29:52 -0400

    Added geometric route distance calculation

---
+++ route_distance.py
@@ -0,0 +1,222 @@
+#Libraries required
+import osmnx as ox
+import networkx as nx
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+import shapely
+import numpy as np
+import pickle
+
+#abbreviate frequently used functions
+from networkx import shortest_path as nx_shortest_path
+from shapely.geometry import Point
+from shapely.geometry import LineString
+from shapely.ops import substring
+from osmnx.distance import nearest_edges
+from osmnx.distance import great_circle_vec
+from osmnx.utils_graph import get_route_edge_attributes
+
+def compute_linestring_length(ls):
+    '''
+    Computes the length of a partial edge (shapely linesting)
+    
+    Parameters
+    ----------
+    ls : shapely.geometry.linestring.LineString
+    Returns
+    -------
+    float : partial edge length distance in meters
+    '''
+
+    if type(ls) == LineString:
+        x, y = zip(*ls.coords)
+    
+        dist = 0
+        for i in range(0, len(x)-1):
+            dist += great_circle_vec(y[i], x[i], y[i+1], x[i+1])
+        return dist
+    else: return None
+
+
+def compute_taxi_length(G, nx_route, orig_partial_edge, dest_partial_edge):
+    '''
+    Computes the route complete taxi route length
+    '''
+
+    dist = 0
+    if nx_route:
+        dist += sum(get_route_edge_attributes(G, nx_route, 'length'))
+    if orig_partial_edge:
+        dist += compute_linestring_length(orig_partial_edge)
+    if dest_partial_edge:
+        dist += compute_linestring_length(dest_partial_edge)
+    return dist
+
+
+def get_edge_geometry(G, edge):
+    '''
+    Retrieve the points that make up a given edge.
+    
+    Parameters
+    ----------
+    G : networkx.MultiDiGraph
+        input graph
+    edge : tuple
+        graph edge unique identifier as a tuple: (u, v, key)
+    
+    Returns
+    -------
+    list :
+        ordered list of (lng, lat) points.
+    
+    Notes
+    -----
+    In the event that the 'geometry' key does not exist within the
+    OSM graph for the edge in question, it is assumed then that 
+    the current edge is just a straight line. This results in an
+    automatic assignment of edge end points.
+    '''
+    
+    if G.edges.get(edge, 0):
+        if G.edges[edge].get('geometry', 0):
+            return G.edges[edge]['geometry']
+    
+    if G.edges.get((edge[1], edge[0], 0), 0):
+        if G.edges[(edge[1], edge[0], 0)].get('geometry', 0):
+            return G.edges[(edge[1], edge[0], 0)]['geometry']
+
+    return LineString([
+        (G.nodes[edge[0]]['x'], G.nodes[edge[0]]['y']),
+        (G.nodes[edge[1]]['x'], G.nodes[edge[1]]['y'])])
+
+
+def shortest_path(G, orig_yx, dest_yx, orig_edge=None, dest_edge=None):
+    '''
+    Parameters
+    ----------
+    G : networkx.MultiDiGraph
+        input graph
+    orig_yx : tuple
+        the (lat, lng) or (y, x) point representing the origin of the path
+    dest_yx : tuple
+        the (lat, lng) or (y, x) point representing the destination of the path
+    
+    Returns
+    -------
+    tuple
+        (route_dist, route, orig_edge_p, dest_edge_p)
+    '''
+    
+    # determine nearest edges
+    if not orig_edge: orig_edge = nearest_edges(G, orig_yx[1], orig_yx[0])
+    if not dest_edge: dest_edge = nearest_edges(G, dest_yx[1], dest_yx[0])
+    
+    orig_partial_edge = [] 
+    dest_partial_edge = []
+    
+    # routing along same edge
+    if orig_edge == dest_edge:        
+        p_o, p_d = Point(orig_yx[::-1]), Point(dest_yx[::-1])
+        edge_geo = get_edge_geometry(G, orig_edge)
+        orig_clip = edge_geo.project(p_o, normalized=True)
+        dest_clip = edge_geo.project(p_d, normalized=True)
+        orig_partial_edge = substring(edge_geo, orig_clip, dest_clip, normalized=True)  
+        dest_partial_edge = []
+        nx_route = []
+    
+    # routing across multiple edges
+    else:
+        p_o, p_d = Point(orig_yx[::-1]), Point(dest_yx[::-1])
+        orig_geo = get_edge_geometry(G, orig_edge)
+        dest_geo = get_edge_geometry(G, dest_edge)
+        orig_clip = orig_geo.project(p_o, normalized=True)
+        dest_clip = dest_geo.project(p_d, normalized=True)
+        # nx_route = nx_shortest_path(G, orig_edge[int(orig_clip+0.5)],
+        #                             dest_edge[int(dest_clip+0.5)], 'length')
+
+        if G.edges[orig_edge]['oneway'] == True:
+            # print(int(dest_clip+0.5))
+            nx_route = nx_shortest_path(G, orig_edge[1], dest_edge[0], 'length')
+        
+        else:
+            # print('ff')
+            nx_route = nx_shortest_path(G, orig_edge[0], dest_edge[0], 'length')
+        
+
+        
+        orig_partial_edge_1 = substring(orig_geo, orig_clip, 1, normalized=True)
+        orig_partial_edge_2 = substring(orig_geo, 0, orig_clip, normalized=True)
+        dest_partial_edge_1 = substring(dest_geo, dest_clip, 1, normalized=True)
+        dest_partial_edge_2 = substring(dest_geo, 0, dest_clip, normalized=True)        
+
+        # when the nx route is just a single node, this is a bit of an edge case
+        if len(nx_route) <= 2:
+            nx_route = []
+            if orig_partial_edge_1.intersects(dest_partial_edge_1):
+                orig_partial_edge = orig_partial_edge_1
+                dest_partial_edge = dest_partial_edge_1
+                
+            if orig_partial_edge_1.intersects(dest_partial_edge_2):
+                orig_partial_edge = orig_partial_edge_1
+                dest_partial_edge = dest_partial_edge_2
+                
+            if orig_partial_edge_2.intersects(dest_partial_edge_1):
+                orig_partial_edge = orig_partial_edge_2
+                dest_partial_edge = dest_partial_edge_1
+                
+            if orig_partial_edge_2.intersects(dest_partial_edge_2):
+                orig_partial_edge = orig_partial_edge_2
+                dest_partial_edge = dest_partial_edge_2
+        
+        
+        # when routing across two or more edges
+        if len(nx_route) >= 3:
+            nx_route_tmp = nx_route
+            ### resolve origin
+            node1 = nx_route_tmp[0]
+            node2 = nx_route_tmp[1]
+            # check overlap with first route edge
+            route_orig_edge = get_edge_geometry(G, (node1, node2, 0))
+            if route_orig_edge.intersects(orig_partial_edge_1) and route_orig_edge.intersects(orig_partial_edge_2):
+                node1 = nx_route_tmp[1]
+                node2 = nx_route_tmp[2]
+                nx_route = nx_route[1:]
+        
+            # determine which origin partial edge to use
+            route_orig_edge = get_edge_geometry(G, (node1, node2, 0)) 
+            if route_orig_edge.intersects(orig_partial_edge_1):
+                orig_partial_edge = orig_partial_edge_1
+            else:
+                orig_partial_edge = orig_partial_edge_2
+
+            ### resolve destination
+            node1 = nx_route_tmp[-2]
+            node2 = nx_route_tmp[-1]
+            # check overlap with last route edge
+            route_dest_edge = get_edge_geometry(G, (node1, node2, 0))
+            if route_dest_edge.intersects(dest_partial_edge_1) and route_dest_edge.intersects(dest_partial_edge_2):
+                node1 = nx_route_tmp[-3]
+                node2 = nx_route_tmp[-2]
+                nx_route = nx_route[:-1]
+
+            # determine which destination partial edge to use
+            
+            route_dest_edge = get_edge_geometry(G, (node1, node2, 0)) 
+            if route_dest_edge.intersects(dest_partial_edge_1):
+                dest_partial_edge = dest_partial_edge_1
+            else:
+                dest_partial_edge = dest_partial_edge_2
+
+    # final check
+    if orig_partial_edge:
+        if len(orig_partial_edge.coords) <= 1:
+            orig_partial_edge = []
+    if dest_partial_edge:
+        if len(dest_partial_edge.coords) <= 1:
+            dest_partial_edge = []
+
+    # compute total path length
+    route_dist = compute_taxi_length(G, nx_route, orig_partial_edge, dest_partial_edge)
+
+    return route_dist, nx_route, orig_partial_edge, dest_partial_edge
